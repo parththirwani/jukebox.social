0a9775a476265a44bb99ec482957f34e0fb227b7
parththirwani <124462644+parththirwani@users.noreply.github.com>
2025-06-15 20:21:23 +0530

    next-auth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,25 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+    })
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token
+      }
+      return token
+    },
+    async session({ session, token }) {
+      return session
+    },
+  },
+  debug: process.env.NODE_ENV === 'development',
+})
+
+export { handler as GET, handler as POST }
--- app/components/Appbar.tsx
+++ app/components/Appbar.tsx
@@ -3,16 +3,31 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 
 export function Appbar() {
-
     const session = useSession();
 
-    return <div>
-        <div className="flex justify-between">
-            Jukebox.Social
+    return (
+        <div className="flex justify-between items-center p-4">
+            <div>
+                Jukebox.Social
+            </div>
+            <div>
+                {session.data?.user && (
+                    <button 
+                        className="m-2 p-2 bg-blue-400 text-white rounded" 
+                        onClick={() => signOut()}
+                    >
+                        Logout
+                    </button>
+                )}
+                {!session.data?.user && (
+                    <button 
+                        className="m-2 p-2 bg-blue-400 text-white rounded" 
+                        onClick={() => signIn()}
+                    >
+                        Sign in
+                    </button>
+                )}
+            </div>
         </div>
-        <div>
-            {session.data?.user && <button className="m-2 p-2 bg-blue-400" onClick={() => signOut}>Logout</button>}
-            {!session.data?.user && <button className="m-2 p-2 bg-blue-400" onClick={() => signIn}>Sign in</button>}
-        </div>
-    </div>
+    );
 }
